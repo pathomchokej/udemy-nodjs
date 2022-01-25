10e78a268027fd58b9d49829c77c672485b775eb
Pathomchoke J <pathomchoke.j@gmail.com>
2022-01-25 15:20:34 +0700

    leason 12-13

---
+++ playground/first-app.js
@@ -0,0 +1,4 @@
+console.log('hello Node.JS');
+
+const fs = require('fs');
+fs.writeFileSync('hello.txt', 'hello Node.JS');
---
+++ playground/test.js
@@ -0,0 +1,8 @@
+var name = 'Max';
+console.log(name);
+
+function summarizeUser(userName, userAge, userHobby) {
+    return 'Name is ' + userName + ', age is ' + userAge + ', hobby is ' + userHobby;
+}
+
+console.log(summarizeUser(name, 10, 'play game'));
