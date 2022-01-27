7a1dc72f68ec9e36d8d156aad968bca85daae382
Pathomchoke J <pathomchoke.j@gmail.com>
2022-01-27 13:31:02 +0700

    14-16

--- playground/test.js
+++ playground/test.js
@@ -1,8 +1,20 @@
 var name = 'Max';
 console.log(name);
 
-function summarizeUser(userName, userAge, userHobby) {
+const summarizeUser = (userName, userAge, userHobby) => {
     return 'Name is ' + userName + ', age is ' + userAge + ', hobby is ' + userHobby;
 }
 
-console.log(summarizeUser(name, 10, 'play game'));
+console.log(summarizeUser(name, 20, 'play game'));
+const person = {
+    name : 'MMM',
+    age : 30,
+    greet : () => 'Hi, I\'m ' + this.name 
+}
+
+console.log(person.greet());
+
+const hobbies = ['Sports', 'Cooking', true, person];
+console.log(hobbies);
+let i = 0;
+console.log(hobbies.map( hobby => '[' + (++i) + '] : ' + hobby));
